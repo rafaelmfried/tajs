6ad00605267d12164ca94ee8205a9b1bd00bb403
Rafael Moura Friederick <rafael.mf.documents@gmail.com>
2024-10-30 13:10:18 -0300

    feat: create spies and stubs to mock the database interface and to evaluate if the correct functions has called

--- test/service.test.js
+++ test/service.test.js
@@ -1,16 +1,18 @@
 import { describe, it, expect, beforeEach, jest} from '@jest/globals';
 import Service from '../src/service.js';
+import crypto from 'node:crypto';
 import fs from 'fs/promises';
 
 describe('Service Test Suite', () => {
   let _service;
   const filename = 'testfile.ndsjon';
-
-  beforeEach(() => {
-    _service = new Service({ filename });
-  });
+  const MOCKED_HASH_PWD = 'hashedpassword';
 
   describe('#read', () => {
+    beforeEach(() => {
+      _service = new Service({ filename });
+    });
+
     it('should return an empty array if the file is empty', async () => {
       jest.spyOn(
         fs,
@@ -58,6 +60,69 @@ describe('Service Test Suite', () => {
       expect(result).toEqual(expected);
 
       // Criar um caso de teste para quando o arquivo n existir. fs/Sync -> fsExists(filename)
+    });
+  });
+
+  describe('#create - spies', () => {
+    beforeEach(() => {
+      jest.spyOn(
+        crypto,
+        crypto.createHash.name
+      )
+      .mockReturnValue({
+        update: jest.fn().mockReturnThis(),
+        digest: jest.fn().mockReturnValue(MOCKED_HASH_PWD),
+  
+      })
+  
+      jest.spyOn(
+        fs,
+        fs.appendFile.name
+      )
+      .mockResolvedValue()
+  
+      _service = new Service({ filename });
+    });
+
+    it('should call appendFile with right params', async () => {
+      /// Arrange
+      const input = {
+        username: 'user_1',
+        password: 'password_1'
+      };
+
+      const expectedCreatedAt = new Date().toISOString();
+
+      jest.spyOn(
+        Date.prototype,
+        Date.prototype.toISOString.name
+      )
+      .mockReturnValue(expectedCreatedAt);
+      // Act
+      await _service.create(input);
+      // Assert
+      // Assert Hash Password Function pois usa no create.
+      expect(crypto.createHash).toHaveBeenNthCalledWith(1, 'sha256');
+
+      const hash = crypto.createHash('sha256')
+
+      expect(hash.update).toHaveBeenCalledWith(input.password);
+      
+      expect(hash.digest).toHaveBeenCalledWith('hex');
+
+      // Assert do create function
+
+      const expected = JSON.stringify({
+        ...input,
+        createdAt: expectedCreatedAt,
+        password: MOCKED_HASH_PWD 
+      }).concat('\n');
+
+      expect(fs.appendFile).toHaveBeenCalledWith(
+        filename,
+        expected
+      );
+
     });
   });
    
